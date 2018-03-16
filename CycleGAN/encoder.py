1d77dd54e3c0aefb0afac395e9723407c5601c7b
cute apple <hidden>
2018-03-16 17:30:02 +0800

    not sure

--- CycleGAN/encoder.py
+++ CycleGAN/encoder.py
@@ -2,7 +2,7 @@ import keras
 import classifier
 
 number_of_class = 5
-input_shape=(256,256,3+number_of_class)
+input_shape=(256,256,3)
 model_path = 'xphoto_G.h5'
 train_data_folder = 'x2photo/train'
 test_data_folder = 'x2photo/test'
@@ -14,21 +14,37 @@ import os
 model_path = os.path.join(os.path.dirname(__file__), model_path)
 del os
 
-def new_G():
+def encode_label(label,output_shape):
 	from keras.models import Sequential
-	from keras.layers import Conv2D,Flatten,Dense,Dropout, LeakyReLU,UpSampling2D,Reshape
+	from keras.layers import Flatten,Dense,LeakyReLU,Reshape
 	import numpy
-
-	preprocess_layers = [
-		Dense(number_of_class*10,input_shape=(number_of_class,)),
+	
+	layers=[
+		Dense(number_of_class*10),
 		LeakyReLU(),
-		Dense(numpy.prod(input_shape),activation='relu'),
+		Dense(numpy.prod(output_shape)),
 		LeakyReLU(),
-		Reshape(input_shape)
+		Reshape(output_shape)
 		]
+	y = label
+	for l in layers:
+		y = l(y)
+	return y
+
+
+def new_G():
+	from keras.models import Sequential,Model
+	from keras.layers import Conv2D,Flatten,Dense,Dropout, LeakyReLU,UpSampling2D,Reshape,Lambda,Input,Concatenate
+
+	input_label = Input((number_of_class,))
+	input_image = Input((256,256,3))
+
+	label_1 = encode_label(input_label,(256,256,4))
+	input_x = Concatenate(3)([label_1,input_image])
 
 	layers = [
-		Conv2D(32,3,strides=1,padding='same',input_shape=input_shape),
+		Lambda(lambda x:x/255,input_shape=(256,256,7)),
+		Conv2D(32,3,strides=1,padding='same'),
 		LeakyReLU(),
 		Conv2D(32,3,strides=2,padding='same'), #1/2 128
 		LeakyReLU(),
@@ -52,8 +68,10 @@ def new_G():
 		UpSampling2D(),
 		Conv2D(16,3,strides=1,padding='same',activation = 'sigmoid'),
 		Conv2D(3,3,strides=1,padding='same',activation = 'sigmoid'),
+		Lambda(lambda x:x*255),
 		]
-	model = Sequential(layers=layers, name='xphoto_G')
+	model = Sequential(layers=layers)
+	model = Model([input_image,input_label],[model(input_x)], name='xphoto_G')
 	return model
 
 def new_model(compile = True):
@@ -61,19 +79,25 @@ def new_model(compile = True):
 	D = classifier.load_model(for_test=True)
 	D.trainable=False
 	i = keras.layers.Input(input_shape)
-	GAN= keras.models.Model(i,D(G(i)))
+	iL = keras.layers.Input((number_of_class,))
+	GAN= keras.models.Model([i,iL],D(G([i,iL])))
 	if compile:
 		GAN.compile(optimizer='RMSProp', loss='mse' ,metrics=['accuracy'])
 	return GAN,G,D
 
 def dataGenerator(path):
 	from keras.preprocessing import image
+	import numpy as np
 	imG = image.ImageDataGenerator(data_format = 'channels_last')
-	return imG.flow_from_directory(path, class_mode='categorical',target_size = input_shape[:2], batch_size = batch_size)
+	imG = imG.flow_from_directory(path, class_mode='categorical',target_size = input_shape[:2], batch_size = batch_size)
+	while True:
+		images, labels = next(imG) # ([batch_size,img],[batch_size,label one hot])
+		random_labels = np.eye(number_of_class)[np.random.choice(number_of_class,batch_size)]
+		yield [images, random_labels], [random_labels]
 
-def save_model(model):
+def save_model(G):
 	print('saving {}'.format(model_path))
-	model.save_weights(model_path)
+	G.save_weights(model_path)
 
 def load_model(for_test = True):
 	GAN,G,D = new_model(compile = not for_test)
@@ -85,7 +109,7 @@ def load_model(for_test = True):
 			raise
 		else:
 			print('load weights failed, recreate')
-	return model
+	return GAN,G,D
 
 def train_model(model):
 	model.fit_generator(
