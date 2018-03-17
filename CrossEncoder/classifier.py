4f52616bfbb2d0e8ce1edde5d0ca7afd5733ef92
cute apple <hidden>
2018-03-17 16:20:23 +0800

    no message

--- CrossEncoder/classifier.py
+++ CrossEncoder/classifier.py
@@ -1,76 +1,61 @@
-import keras
-from keras.models import Sequential,Model
-from keras.layers import *
+file_path='C.h5'
 
-input_shape=(28,28,1)
-model_path = 'mnist_classifier.h5'
-epochs = 50
+
+# from https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
 batch_size = 128
 num_classes = 10
+epochs = 50
 
-import os
-model_path = os.path.join(os.path.dirname(__file__), model_path)
-del os
+# input image dimensions
+img_rows, img_cols = 28, 28
 
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+	
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
 
-def new_model():
-	model = Sequential(name='mnist_classifier')
-	model.add(Conv2D(32, kernel_size=3, strides=1, activation='relu',input_shape=input_shape))
-	model.add(Conv2D(64, kernel_size=3, strides=2, activation='relu'))
-	model.add(Dropout(0.25))
-	model.add(Flatten())
-	model.add(Dense(128, activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(Dense(num_classes))
-	model.compile(optimizer='adadelta', loss='categorical_crossentropy' ,metrics=['accuracy'])
-	return model
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 
-loaded_mnist = False
-def load_mnist():
-	global loaded_mnist
-	if not loaded_mnist:
-		from keras.datasets import mnist
-		def transform(x):
-			return x.astype('float32').reshape(-1,*input_shape)/255
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
-		(x_train, y_train), (x_test, y_test) = mnist.load_data()
-		x_train = transform(x_train)
-		x_test = transform(x_test)
-		y_train = keras.utils.to_categorical(y_train, 10)
-		y_test = keras.utils.to_categorical(y_test, 10)
-		loaded_mnist = (x_train,y_train),(x_test,y_test)
-	return loaded_mnist
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+					activation='relu',
+					input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
 
-def load_model(new_on_fail=True):
-	model = new_model()
-	try:
-		print('loading {}'.format(model_path))
-		model.load_weights(model_path)
-	except (OSError,ValueError) as e:
-		if not new_on_fail:
-			print('load weights failed')
-			raise SystemExit()
-		else:
-			print('load weights failed, recreate')
-	return model
+model.compile(loss=keras.losses.categorical_crossentropy,
+				optimizer=keras.optimizers.Adadelta(),
+				metrics=['accuracy'])
 
-def train_model(model):
-	(x_train,y_train),(x_test,y_test) = load_mnist()
-	model.fit(x_train, y_train,
-			  batch_size=batch_size,
-			  epochs=epochs,
-			  verbose=1,
-			  validation_data=(x_test, y_test))
+model.fit(x_train, y_train,
+			batch_size=batch_size,
+			epochs=epochs,
+			verbose=1,
+			validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
 
-def save_model(model):
-	print('saving {}'.format(model_path))
-	model.save_weights(model_path)
-
-def main():
-	model = load_model(new_on_fail=True)
-	train_model(model)
-	save_model(model)
-
-if __name__ == '__main__':
-	main()
-
+model.save(file_path)
