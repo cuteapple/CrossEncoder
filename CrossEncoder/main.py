8b18ea901a5b8a153176a435392d4077ac70c15b
cute apple <hidden>
2018-03-22 10:20:20 +0800

    save/load

--- CrossEncoder/main.py
+++ CrossEncoder/main.py
@@ -63,17 +63,30 @@ y51 = yab(5,3)
 #G15 = img.flow(x1,y15,batch_size=256)
 #G51 = img.flow(x1,y15,batch_size=256)
 
+try:
+	M11.load_weights('auto1_Weight.h5')
+	M55.load_weights('auto5_Weight.h5')
+	print('load weight success')
+except:
+	print('load weight fail')
+	pass
+
 for i in range(100):
-	verbose = 0
+	verbose = 1
 	auto_epoch = 3
 	cross_epoch = 1
+	print()
 	print(' -- epoch {} -- '.format(i))
+	print()
 	M11.fit(x1,x1,batch_size=128, epochs=auto_epoch,verbose=verbose)
 	M55.fit(x5,x5,batch_size=128, epochs=auto_epoch,verbose=verbose)
 	M15.fit(x1,y15,batch_size=128, epochs=cross_epoch,verbose=verbose)
 	M51.fit(x5,y51,batch_size=128, epochs=cross_epoch,verbose=verbose)
 
+M11.save_weights('auto1_Weight.h5')
+M55.save_weights('auto5_Weight.h5')
+
 Model(i1,auto1).save('M11.h5')
 Model(i1,cross15).save('M15.h5')
-Model(i5,auto5).save('M55.h5')
+Model(i5,auto5).save('M55.h5')`
 Model(i5,cross51).save('M51.h5')
