1e650b46a0d6b0bc91fcb691c82a7d19645fa4c5
cute apple <hidden>
2018-03-22 17:45:15 +0800

    basic struct

--- CrossGAN/main.py
+++ CrossGAN/main.py
@@ -1,14 +1,17 @@
 import keras
-import argparse
+import keras_contrib
 
 class AutoEncoder():
-	def __init__(self,compile=True):
+	def __init__(self):#,compile=True):
 
 		#parameters
 
 		self.width = 128
 		self.height = 128
-		
+		# parameter for [encoder, decoder]
+		self.deeps = [[32,64,128,256],[128,64,32,3]]
+		self.nres = [3,3]
+
 		#build graph
 
 		self.ioshape = (self.width, self.height, 3)
@@ -23,9 +26,9 @@ class AutoEncoder():
 		self.z = self.encoder(self.i)
 		self.o = self.decoder(self.z)
 
-		self.autoencoder = keras.models.Model(self.i,self.o)
-		if compile:
-			self.autoencoder.compile('SGD',loss='mse',metrics=['accuracy'])
+		#self.autoencoder = keras.models.Model(self.i,self.o)
+		#if compile:
+		#	self.autoencoder.compile('sgd',loss='mse',metrics=['accuracy'])
 
 	def save(file):
 		self.autoencoder.save_weights(file)
@@ -34,13 +37,67 @@ class AutoEncoder():
 		self.autoencoder.load_weights(file)
 
 	def newEncoder(self):
-		from keras.layers import Input,Conv2D
-		i = Input(self.ioshape)
-		layers = []
+		''' brand new encoder '''
+		
+		input_shape = self.ioshape
+		nres, deeps = self.nres[0], self.deeps[0]
+
+		y = i = keras.layers.Input(input_shape)
+
+		for d in deeps:
+			y = self.convdown(y,d)
+
+		for _ in range(nres):
+			y = self.convres(y,deeps[-1])
+
+		return keras.models.Model(i,y)
 
 	def newDecoder(self):
-		...
+		''' brand new decoder '''
 
+		input_shape = self.z_shape
+		nres, deeps = self.nres[1], self.deeps[1]
+
+		y = i = keras.layers.Input(input_shape)
+
+		for _ in range(nres):
+			y = self.convres(y, input_shape[-1])
+
+		for deep in deeps:
+			y = self.convup(y,deep)
+
+		return keras.models.Model(i,y)
+
+	@staticmethod
+	def convdown(x,deep,kernal=(5,5)):
+		''' conv 1/2 -> lrelu -> instanceNorm '''
+		from keras.layers import Conv2D,LeakyReLU
+		from keras_contrib.layers.normalization import InstanceNormalization
+		x = Conv2D(deep, kernel_size=kernal, strides=2, padding='same')(x)
+		x = LeakyReLU(alpha=0.2)(x)
+		x = InstanceNormalization()(x)
+		return x
+
+	@staticmethod
+	def convup(x,deep,kernal=(5,5)):
+		''' upsample -> conv -> lrelu -> instanceNorm '''
+		from keras.layers import Conv2D,LeakyReLU,UpSampling2D
+		from keras_contrib.layers.normalization import InstanceNormalization
+		x = UpSampling2D()(x)
+		x = Conv2D(deep, kernel_size=kernal, strides=1, padding='same')(x)
+		x = LeakyReLU(alpha=0.2)(x)
+		x = InstanceNormalization()(x)
+		return x
+
+	@staticmethod
+	def convres(x,deep,kernal=(3,3)):
+		from keras.layers import Conv2D,Add,LeakyReLU
+		y = Conv2D(deep, kernel_size=kernal, strides=1, padding='same')(x)
+		y = LeakyReLU(alpha=0.2)(y)
+		y = Conv2D(deep, kernel_size=kernal, strides=1, padding='same')(y)
+		y = Add()([x,y])
+		y = LeakyReLU(alpha=0.2)(y)
+		return y
 
 def main():
 	a = AutoEncoder()
@@ -49,8 +106,8 @@ def main():
 	a2b = b.decoder(a.z)
 	b2a = a.decoder(b.z)
 
+	from keras.utils import plot_model
+	plot_model(a.encoder, to_file='e.png',show_shapes =True)
+	plot_model(a.decoder, to_file='d.png',show_shapes =True)
 
-
-	y = b.autoencoder
-	z = a.decoder(b.encoder)
-	w = b.encoder(a.decoder)
+main()
