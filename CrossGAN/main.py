a4301d6380336f2aec0ce10b488a1d99932f6508
cute apple <hidden>
2018-03-22 16:03:44 +0800

    ...

--- CrossGAN/main.py
+++ CrossGAN/main.py
@@ -1,58 +1,56 @@
 import keras
+import argparse
 
 class AutoEncoder():
-	def __init__(self):
+	def __init__(self,compile=True):
+
+		#parameters
+
 		self.width = 128
 		self.height = 128
 		
 		#build graph
+
 		self.ioshape = (self.width, self.height, 3)
 		self.encoder = self.newEncoder()
 		self.z_shape = self.encoder.output_shape[1:]
 		self.decoder = self.newDecoder()
 		assert self.ioshape == self.decoder.output_shape[1:]
-		#self.discriminator = self.newDiscriminator()
 		
 		#some easy-to-use things 
+		
 		self.i = keras.layers.Input(self.ioshape)
 		self.z = self.encoder(self.i)
 		self.o = self.decoder(self.z)
 
 		self.autoencoder = keras.models.Model(self.i,self.o)
-		self.autoencoder.compile('SGD','')
+		if compile:
+			self.autoencoder.compile('SGD',loss='mse',metrics=['accuracy'])
 
 	def save(file):
-		self.encoder.save_weights(file)
-		self.decoder.save_weights(file)
+		self.autoencoder.save_weights(file)
 	
 	def load(file):
-		self.encoder.load_weights(file)
-		self.decoder.load_weights(file)
+		self.autoencoder.load_weights(file)
 
 	def newEncoder(self):
-		...
+		from keras.layers import Input,Conv2D
+		i = Input(self.ioshape)
+		layers = []
 
 	def newDecoder(self):
 		...
 
-	def link_and_compile(self):
-		enc = self.encoder
-		dec = self.decoder
-		#D = self.discriminator
-		from keras.layers import Input
-		i = Input(shape = (self.width, self.height, 3))
-		o = dec(enc(i))
-		return keras.models.Model(i,o)
+
+def main():
+	a = AutoEncoder()
+	b = AutoEncoder()
+
+	a2b = b.decoder(a.z)
+	b2a = a.decoder(b.z)
 
 
-a = AutoEncoder()
-b = AutoEncoder()
 
-ia = Input(a.ioshape)
-ib = Input(b.ioshape)
-
-
-x = a.decoder(a.encoder(ia))
-y = b.autoencoder
-z = a.decoder(b.encoder)
-w = b.encoder(a.decoder)
+	y = b.autoencoder
+	z = a.decoder(b.encoder)
+	w = b.encoder(a.decoder)
