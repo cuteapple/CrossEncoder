98650bd8212556245ea1a9b1875b718edb670761
cute apple <hidden>
2018-04-05 11:00:09 +0800

    update test

--- DCGAN/test_G.py
+++ DCGAN/test_G.py
@@ -1,6 +1,6 @@
 import dcgan
 import numpy as np
-_,G,_ = dcgan.load_model(False)
+_,G,D = dcgan.load_model(False)
 
 def zG():
 	for items in dcgan.z_generator():
@@ -13,5 +13,8 @@ import cv2
 for i in range(100):
 	x = next(zG)
 	p = G.predict(x[0])
+	c = D.predict(p)
+	print(x[1])
+	print(c)
 	p = p.reshape(28,28)*255
-	cv2.imwrite('{}-{}.png'.format(i,x[1]),p)
+	cv2.imwrite('o/{}-{}.png'.format(i,x[1]),p)
