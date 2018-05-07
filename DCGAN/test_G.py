2dabdd0ee2d65b478c961a3d774f2ea1032521e2
cute apple <hidden>
2018-05-07 14:08:35 +0800

    fix test

--- DCGAN/test_G.py
+++ DCGAN/test_G.py
@@ -21,21 +21,21 @@ g.load_weights(args.path)
 z,y = next(G.z(100,20))
 x = g.predict(z)
 
-sample = [np.zeros((32,32,3))]*10
+sample = [np.zeros((28,28))]*10
 
 import cv2
 for i,(p,y) in enumerate(zip(x,y)):
 	y = int(np.dot(y,np.arange(10)))
-	p = p.reshape(32,32,3)*255
+	p = p.reshape(28,28)*255
 	sample[y] = p
 	cv2.imwrite('{}/{}-{}.png'.format(o,y,i),p)
 
 margin = 1
-canvas = np.ones((32*2+3,32*5+6),np.uint8)*128
+canvas = np.ones((28*2+3,28*5+6),np.uint8)*128
 for i,im in enumerate(sample):
 	nx = i//5
 	ny = i%5
-	x = nx*33+1
-	y = ny*33+1
-	canvas[x:x+32,y:y+32]=im
+	x = nx*29+1
+	y = ny*29+1
+	canvas[x:x+28,y:y+28]=im
 	cv2.imwrite('{}/sample.png'.format(o),canvas)
