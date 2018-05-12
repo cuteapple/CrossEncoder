bbf7ff854c9f837431f69ea0627d543093dfef9b
cute apple <hidden>
2018-05-12 09:45:12 +0800

    show test data

--- DCGAN/sample_NoisyData.py
+++ DCGAN/sample_NoisyData.py
@@ -2,10 +2,16 @@ from D import NoizyData
 
 n = NoizyData()
 
-data = n.train_batch(100)
+train_data = n.train_batch(100)
+
+a,b = n.test()
+test_data = [a[:100],b[:100]]
 
 import cv2
 import numpy as np
 
-for i,(im,label) in enumerate(zip(*data)):
-	cv2.imwrite('results/od/'+str(i)+'.png',(im*255).astype(np.uint8))
+for i,(im,label) in enumerate(zip(*train_data)):
+	cv2.imwrite('results/od/train-'+str(i)+'.png',(im*255).astype(np.uint8))
+
+for i,(im,label) in enumerate(zip(*test_data)):
+	cv2.imwrite('results/od/test-'+str(i)+'.png',(im*255).astype(np.uint8))
