6de15e3a9b10b299590f5b139d3647fd685be94a
cute apple <hidden>
2018-05-17 11:35:40 +0800

    [dev] button needs QT !?

--- DCGAN/demo.py
+++ DCGAN/demo.py
@@ -1,37 +1,20 @@
 import cv2
 import numpy as np
 
-def nothing(x):
-    pass
 
-update = True
-z_shape = (20,)
-z = np.random.normal(size=z_shape)
-z[:10]=0
+print('initializing ...')
+import G
 
-# Create a black image, a window
-#img = np.zeros((300,512,3), np.uint8)
-Wcontrols = 'control'
-cv2.namedWindow(Wcontrols,cv2.WINDOW_NORMAL)
-
-Wimg = 'result'
-cv2.namedWindow(Wimg,cv2.WINDOW_AUTOSIZE)
-
-
-# create trackbars for color change
-for i in range(10):
-	def update_i(x,i=i):
-		z[i] = x / 100
-		global update
-		update = True
-		print('set {} to {}'.format(i,z[i]))
-	cv2.createTrackbar(str(i),Wcontrols,0,100, update_i)
 
 print('loading model ...')
-import G
+z_shape = (20,)
 g = G.new_G(z_shape)
 g.load_weights('G.h5')
 
+z = np.random.normal(size=z_shape)
+z[:10] = 0
+
+update = True
 def predict():
 	global im
 	global update
@@ -42,6 +25,27 @@ def predict():
 	print('predict', z)
 	im = g.predict(z.reshape(1,*z_shape)).reshape(28,28,1)
 	im = cv2.resize(im,(280,280))
+
+Wcontrols = 'control'
+Wimg = 'result'
+cv2.namedWindow(Wcontrols,cv2.WINDOW_NORMAL)
+cv2.namedWindow(Wimg,cv2.WINDOW_AUTOSIZE)
+
+
+
+# create trackbars
+for i in range(10):
+	steps = 100 # step of trackbar
+	def update_i(x,i=i):
+		z[i] = x / steps
+		global update
+		update = True
+		print('set {} to {}'.format(i,z[i]))
+	cv2.createTrackbar(str(i),Wcontrols,int(z[i] * steps),steps, update_i)
+	
+def random_z():
+	z[10:] = np.random.normal(size=10)
+cv2.createButton('Random Z',random_z)
 
 while cv2.getWindowProperty(Wcontrols, 0) >= 0:
 	predict()
