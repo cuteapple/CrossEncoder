5e22580ac4a24da228f66d3c221df1d30429cdd0
cute apple <hidden>
2018-05-17 11:13:58 +0800

    show image

--- DCGAN/demo.py
+++ DCGAN/demo.py
@@ -4,6 +4,7 @@ import numpy as np
 def nothing(x):
     pass
 
+update = True
 z_shape = (20,)
 z = np.random.normal(size=z_shape)
 
@@ -16,19 +17,33 @@ Wimg = 'result'
 cv2.namedWindow(Wimg,cv2.WINDOW_AUTOSIZE)
 
 
-
 # create trackbars for color change
 for i in range(10):
 	def update_i(x,i=i):
 		z[i] = x / 100
+		global update
+		update = True
 		print('set {} to {}'.format(i,z[i]))
 	cv2.createTrackbar(str(i),Wcontrols,0,100, update_i)
 
-#import G
-#g = G.new_G(z_shape)
-while(1):
-    k = cv2.waitKey(33) & 0xFF
-    if k == 27:
-        break
+print('loading model ...')
+import G
+g = G.new_G(z_shape)
+g.load_weights('G.h5')
+
+def predict():
+	if not update:
+		return
+	
+	global im
+	im = g.predict(z.reshape(1,*z_shape)).reshape(28,28,1)
+
+while cv2.getWindowProperty(Wcontrols, 0) >= 0:
+	predict()
+
+	cv2.imshow(Wimg,im)
+	k = cv2.waitKey(33) & 0xFF
+	if k == 27:
+		break
 
 cv2.destroyAllWindows()
