3285e55e5082e93743ad523850e7060bf6cc4124
cute apple <hidden>
2018-05-17 15:43:47 +0800

    looks cool

--- DCGAN/demo.py
+++ DCGAN/demo.py
@@ -98,8 +98,73 @@ def draw_z_img():
 		x2 = x1 + int(delta)
 		x1 += 1
 		#print(x1)
-		cv2.putText(z_canvas,str(i),(x1,h-5),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.5,(1,1,1),1,cv2.FILLED,False)
-		
+		cv2.putText(z_canvas,str(i),(x1,h - 5),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.5,(1,1,1),1,cv2.FILLED,False)
+
+
+#
+# graph selection
+#
+def gss():
+
+	w = 32
+	h = 32
+
+	canvas = np.zeros((w,h,3))
+	selection = np.zeros((w,h,10))
+
+	dx3 = w / 4
+	dx4 = w / 5
+	dy = h / 4
+	poss = [[dx3,dy],[dx3 * 2,dy],[dx3 * 3,dy],
+		[dx4,dy * 2],[dx4 * 2,dy * 2],[dx4 * 3,dy * 2],[dx4 * 4,dy * 2],
+		[dx3,dy * 3],[dx3 * 2,dy * 3],[dx3 * 3,dy * 3]]
+	#poss = np.random.uniform(0,50,(10,2))
+
+	for x in range(w):
+		for y in range(h):
+			pos = np.array([x,y])
+			color = np.zeros(3)
+			z = np.zeros(10)
+			weight = 0
+			for i in range(10):
+				dist = np.sqrt(np.sum(np.square(pos - poss[i])))
+				w = 1 / np.sqrt(dist + 1)
+				weight += w
+				color += w * hcolors[i]
+				z[i] =weight
+			color /= weight
+			canvas[y,x] = color
+
+	canvas = cv2.resize(canvas,(0,0),fx=12,fy=12,interpolation=cv2.INTER_CUBIC)
+	cv2.namedWindow('im-selection')
+	cv2.imshow('im-selection',canvas)
+
+	hold = False
+	ix,iy = 0,0
+	def onmouse(event,x,y,flags,param):
+		nonlocal hold,ix,iy
+		if event == cv2.EVENT_LBUTTONDOWN:
+			hold = True
+			ix,iy = x,y
+
+		elif event == cv2.EVENT_MOUSEMOVE:
+			if not hold:
+				return
+			ix,iy = x,y
+
+		elif event == cv2.EVENT_LBUTTONUP:
+			hold = False
+
+		print(ix,iy)
+
+
+	cv2.setMouseCallback('im-selection',onmouse)
+
+gss()		
+	
+
+
+
 while cv2.getWindowProperty(Wcontrols, 0) >= 0:
 	if update:
 		update = False
@@ -111,7 +176,7 @@ while cv2.getWindowProperty(Wcontrols, 0) >= 0:
 	if k == 27:
 		break
 	if k == ord(' '):
-		z[10:]=np.random.normal(0,1,10)
+		z[10:] = np.random.normal(0,1,10)
 		update = True
 
 cv2.destroyAllWindows()
