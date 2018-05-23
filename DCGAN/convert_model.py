225076800a2ecf7994e8d15dfa241a250b44eb91
cute apple <hidden>
2018-05-23 16:14:28 +0800

    remove unused file

--- DCGAN/convert_model.py
+++
@@ -1,9 +0,0 @@
-from argparse import ArgumentParser
-ap = ArgumentParser()
-ap.add_argument('-G',metavar='path',help='path to g weights',default = 'G.h5')
-ap.add_argument('-D',metavar='path',help='path to d weights',default = 'D.h5')
-args = ap.parse_args()
-
-def convertG():
-	import G
-	G.new_G()
