a06b11b35208ffb5d818b2ed9a28443464758972
cute apple <hidden>
2018-06-01 15:00:01 +0800

    classifier

--- DCGAN/D.py
+++ DCGAN/D.py
@@ -2,10 +2,10 @@ import keras
 import numpy as np
 from Dataset import NoizyData
 
-def new_D():
+def number_D():
 	from keras.models import Sequential,Model
-	from keras.layers import Conv2D,Flatten,Dense,Dropout,Input,MaxPooling2D
-	modelc = Sequential(name='classifier',
+	from keras.layers import Conv2D,Flatten,Dense,Dropout,MaxPooling2D
+	modelc = Sequential(name='number',
 		layers=[Conv2D(32, kernel_size=3, strides=1, activation='relu',input_shape=(28,28,1)),
 			Conv2D(64, kernel_size=3, strides=2, activation='relu'),
 			Dropout(0.5),
@@ -17,8 +17,13 @@ def new_D():
 			Dense(128, activation='relu'),
 			Dropout(0.5),
 			Dense(10)])
+	return modelc
 
-	modeln = Sequential(name='isnoisy',
+def noisy_D():
+	from keras.models import Sequential,Model
+	from keras.layers import Conv2D,Flatten,Dense,Dropout,Input,MaxPooling2D
+
+	modeln = Sequential(name='noisy',
 		layers=[Conv2D(32, kernel_size=3, strides=1, activation='relu',input_shape=(28,28,1)),
 			MaxPooling2D(),
 			Conv2D(64, kernel_size=3, activation='relu'),
@@ -30,15 +35,7 @@ def new_D():
 			Dense(128, activation='relu'),
 			Dropout(0.5),
 			Dense(1)])
-	
-	i = Input((28,28,1),name='image')
-	oc = modelc(i)
-	on = modeln(i)
-
-	model = Model(i,[oc,on],name='D')
-	#model.summary()
-
-	return model
+	return modeln
 
 if __name__ == "__main__":
 
@@ -59,11 +56,12 @@ if __name__ == "__main__":
 	
 	print('prepare data ...')
 	data = NoizyData()
+	
 	def g():
 		nreal,nfake = args.batch_size//2, args.batch_size//2
 		while True:
 			yield data.train_batch(nreal,nfake)
-
+		
 	print('training ... ')
 	d.compile(optimizer='adadelta', loss='mse', metrics=['mae','accuracy'])
 	
---
+++ DCGAN/classifier.py
@@ -0,0 +1,49 @@
+import keras
+import numpy as np
+from Dataset import NoizyData
+
+def new_model():
+	from keras.models import Sequential,Model
+	from keras.layers import Conv2D,Flatten,Dense,Dropout,MaxPooling2D,LeakyReLU,Softmax
+	return Sequential(name='class',
+		layers=[Conv2D(32, kernel_size=3, strides=1, padding='same', input_shape=(28,28,1)),
+			LeakyReLU(),
+			Dropout(0.25),
+			Conv2D(64, kernel_size=3, strides=2,padding='same'),
+			LeakyReLU(),
+			Dropout(0.25),
+			Conv2D(128, kernel_size=3, strides=2, padding='same'),
+			LeakyReLU(),
+			Dropout(0.25),
+			Conv2D(256, kernel_size=3, strides=2, padding='same'),
+			LeakyReLU(),
+			Dropout(0.25),
+			Flatten(),
+			Dense(10,activation='sigmoid')])
+
+if __name__ == "__main__":
+
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-e","--epochs", default=200, type=int)
+	parser.add_argument("-b","--batch_size", default=128, type=int)
+	parser.add_argument("-p","--path", default="DC.h5", type=str)
+	args = parser.parse_args()
+	print('args',args)
+
+	dc = new_model()
+	
+	print('loading weights at {} ... '.format(args.path), end = '')
+	try: dc.load_weights(args.path)
+	except: print('fail')
+	else: print('success')
+	
+	print('prepare data ...')
+	x,y,_ = NoizyData.load_mnist()
+
+	print('training ... ')
+	dc.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
+	dc.fit(x,y,batch_size=args.batch_size, epochs = args.epochs)
+	
+	print('saving ...')
+	dc.save_weights(args.path)
