9ff240c413c5a981cdc417f659401c0bc6c37dac
cute apple <hidden>
2018-06-01 15:27:51 +0800

    ...

--- DCGAN/classifier.py
+++ DCGAN/classifier.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 		print('success')
 	
 	print('prepare data ...')
-	x,y,_ = Dataset.load_mnist()
+	(x,y),_ = Dataset.load_mnist()
 
 	print('training ... ')
 	dc.fit(x,y,batch_size=args.batch_size, epochs = args.epochs)
