42fd8e07759a62fc98ac3c9b1e7b95490aeaecb7
cute apple <hidden>
2018-06-11 12:06:11 +0800

    parameterlize

--- DCGAN/statistics.py
+++ DCGAN/statistics.py
@@ -4,13 +4,15 @@ import cv2
 from keras.datasets import mnist
 (x, y), (tx, ty) = mnist.load_data()
 
+f = lambda a: np.max(a,axis=0)
+name = 'max'
+
 z = np.zeros((10,28,28))
 
 for n in range(10):
-	z[n] = np.max(x[y==n],axis=0)
-
+	z[n] = f(x[y==n])
+	
 for n in range(10):
-	cv2.imwrite('{}.png'.format(n),z[n])
+	cv2.imwrite('{}.{}.png'.format(name,n),z[n])
 
-
-cv2.imwrite('all.png',np.amax(z,axis=0))
+cv2.imwrite('{}.all.png'.format(name),f(z))
