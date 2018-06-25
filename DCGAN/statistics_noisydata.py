3205e48a9efa6e9c3b023b636b27324fed5d10bc
cute apple <hidden>
2018-06-25 15:57:14 +0800

    statisics

--- DCGAN/statistics_noisydata.py
+++ DCGAN/statistics_noisydata.py
@@ -1,10 +1,28 @@
 from D import NoizyData
+import numpy as np
 
 x,_ = NoizyData(0.5,0.5,0.3).train()
 
-x = x[len(x) // 2:] #noisy part
-import numpy as np
+from keras.datasets import mnist
+(_, y), _ = mnist.load_data()
+y = np.concatenate([y,y])
 
+c = np.zeros((28 * 3,28 * 11,1))
+
+for w in range(10):
+	target = x[y == w]
+	c[0:28, w * 28:w * 28 + 28] = np.max(target, axis=0)
+	c[28:28 * 2, w * 28:w * 28 + 28] = np.mean(target, axis=0)
+	c[28 * 2:, w * 28:w * 28 + 28] = np.min(target, axis=0)
+
+c[0:28,280:] = np.max(x,axis=0)
+c[28:28 * 2,280:] = np.mean(x,axis=0)
+c[28 * 2:,280:] = np.min(x,axis=0)
+
+import cv2
+cv2.imwrite('nst.png',c*255)
+
+x = x[len(x) // 2:] #noisy part
 nr = 4
 nc = 10
 
