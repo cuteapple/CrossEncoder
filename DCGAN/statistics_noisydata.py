02166cf8562b0ecca70e2a29d3a93b9acedc9b2d
cute apple <hidden>
2018-06-25 12:22:18 +0800

    param

--- DCGAN/statistics_noisydata.py
+++ DCGAN/statistics_noisydata.py
@@ -2,15 +2,17 @@ from D import NoizyData
 
 x,_ = NoizyData(0.5,0.5,0.3).train()
 
-x = x[len(x)//2:] #noisy part
-
+x = x[len(x) // 2:] #noisy part
 import numpy as np
 
-c = np.ones((28*4,28*5,1))*0.5
+nr = 4
+nc = 10
 
-for a in range(4):
-	for b in range(5):
-		c[28*a:28*a+28,28*b:28*b+28]=x[np.random.choice(len(x))]
+c = np.ones((28 * nr,28 * nc,1)) * 0.5
+
+for a in range(nr):
+	for b in range(nc):
+		c[28 * a:28 * a + 28,28 * b:28 * b + 28] = x[np.random.choice(len(x))]
 
 c*=255
 
