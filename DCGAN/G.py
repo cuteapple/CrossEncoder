599e27a31024228b85a8a8533bf0044346a74109
cute apple <hidden>
2018-07-05 17:00:15 +0800

    paper items

--- DCGAN/G.py
+++ DCGAN/G.py
@@ -38,8 +38,10 @@ def new_G(input_shape):
 	return Model(i,x,'G')
 
 
-#from keras.utils import plot_model
-#plot_model(new_G((20,)), show_shapes=True, to_file='G.png')
+from keras.utils import plot_model
+plot_model(new_G((20,)), show_shapes=True, to_file='G.png')
+raise SystemExit(0)
+
 
 def z(batch_size,length):
 	def g():
